8534e2bf8f90128a2385fe65dc253b4677aa1fe8
stephen & John Young <student@epicodus.com>
2015-05-27 11:03:12 -0700

    add spec for multiple words and code to make it pass to fo_shizzle.rb, all specs passing.

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,11 +1,21 @@
 class String
   define_method(:fo_shizzle) do
-    letters = self.split("")
-    letters.each do |letter|
-      if letter.==("s")
+    words = self.split(" ")
+    letters = []
+    letters_new = []
+    words.each do |word|
+    letters = word.split("")
+    letters.each_with_index() do |letter, index|
+      if letter.==("s") && index!=(0)
         letter.replace("z")
+
       end
+      new_word = letters.join()
+
+      word.replace(new_word)
+
     end
-    letters.join()
+    end
+    words.join(" ")
   end
 end
--- spec/fo_shizzle_spec.rb
+++ spec/fo_shizzle_spec.rb
@@ -2,7 +2,12 @@ require('rspec')
 require('fo_shizzle')
 
 describe('String#fo_shizzle') do
-  it('changes all s letters to z, in a string') do
-    expect("she has super blonde hair".fo_shizzle()).to(eq("zhe haz zuper blonde hair"))
+
+  it("will not change first letter of a word form s to z") do
+    expect('supers'.fo_shizzle).to(eq("superz"))
+  end
+
+  it('changes letter s to z unless it is the first letter of a word in a string of multiple words') do
+    expect("she has super blonde hairs".fo_shizzle()).to(eq("she haz super blonde hairz"))
   end
 end
