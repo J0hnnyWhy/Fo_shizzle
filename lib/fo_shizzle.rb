2d8ccc3b93d38e2d18684794f3136a1352556126
John <jayelwhy@yahoo.com>
2015-05-28 08:59:01 -0700

    better version of Fo_shizzle

--- lib/fo_shizzle.rb
+++ lib/fo_shizzle.rb
@@ -1,20 +1,20 @@
 class String
   define_method(:fo_shizzle) do
     words = self.split(" ")
-    letters = []
-    letters_new = []
+
     words.each do |word|
-    letters = word.split("")
-    letters.each_with_index() do |letter, index|
-      if letter.==("s") && index!=(0)
-        letter.replace("z")
+      letters = word.split("")
+
+      letters.each_with_index() do |letter, index|
+        if (letter.==("s") || letter.==("S")) && index!=(0)
+          letter.replace("z")
+
+        end
+        new_word = letters.join()
+
+        word.replace(new_word)
 
       end
-      new_word = letters.join()
-
-      word.replace(new_word)
-
-    end
     end
     words.join(" ")
   end
